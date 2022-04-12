ac75c0a7080adf8140455399430005299482ee7a
mike489 <michaeltesfaye84@gmail.com>
2022-04-12 02:54:24 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+	char result[] = "_putchar";
+
+	int c;
+
+	for (c = 0; a < 8; c++)
+	{
+		_putchar(result[a]);
+	}
+	-putchar("\n");
+	return (0);
+}
+
