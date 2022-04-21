1697f86c70d192d45b19e60d0a92cb14cf5155f9
mike489 <michaeltesfaye84@gmail.com>
2022-04-21 20:00:06 +0300

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reverse_array  - reverses the content of an array
+ * reverse_array - reverses the content of an array
  * of integers.
  * @a: array.
  * @n: number of elements of the array.
@@ -11,12 +11,12 @@ void reverse_array(int *a, int n)
 {
 	int i, j, temp;
 
-	fro (i = 0; i < n - 1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		for (j = i + 1; j > 0; j--)
 		{
 			temp = *(a + j);
-			*(a + j) = *(a+(j - 1));
+			*(a + j) = *(a + (j - 1));
 			*(a + (j - 1)) = temp;
 		}
 	}
