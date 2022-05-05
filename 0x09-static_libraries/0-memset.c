02bb25b26a57ec94a26872efd9cac312c06f4eb0
mike489 <michaeltesfaye84@gmail.com>
2022-05-05 22:50:28 +0300

    g

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-char *_memset(char *s, char b, unsigned int n)
+int _isupper(int c)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (s);
+	return (c >= 'A' && c <= 'Z');
 }
