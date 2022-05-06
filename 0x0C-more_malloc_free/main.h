ae86ad9fe33529cb95db5326cdac2602a9c0e804
mike489 <michaeltesfaye84@gmail.com>
2022-05-06 20:53:46 +0300

    all

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
+void _is_zero(char *argv[])
+char *_initialize_array(char *ar, int lar)
+int _checknum(char *argv[], int n)	
+int main(int argc, char *argv[])	
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
