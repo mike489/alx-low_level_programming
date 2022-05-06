d9024ba7f8c5d444105dd1ca55aa308a964ac0e0
mike489 <michaeltesfaye84@gmail.com>
2022-05-06 21:40:37 +0300

    all

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,14 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
-void _is_zero(char *argv[])
-char *_initialize_array(char *ar, int lar)
-int _checknum(char *argv[], int n)	
-int main(int argc, char *argv[])	
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
