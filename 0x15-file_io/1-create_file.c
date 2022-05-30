bb990830b03ecc3175def5e70f432f3f1ca84027
mike489 <michaeltesfaye84@gmail.com>
2022-05-30 20:07:04 +0300

    edit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+
